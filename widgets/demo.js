d531736c7bbc35febf35466f211eabeeebdec608
Sirichat Sangjuiwong <76915370+devpae@users.noreply.github.com>
2025-01-19 02:28:54 +0700

    Create demo.js

---
+++ widgets/demo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DemoWidget: React.FC = () => {
+  return (
+    <div>
+      <h2>Demo Widget</h2>
+      <p>This is a dynamically loaded widget.</p>
+    </div>
+  );
+};
+
+export default DemoWidget;
